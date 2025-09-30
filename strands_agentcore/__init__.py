35bd8eca833be88445c6e4abd31e617288891521
samirmaske23 <samirmaske23091998@gaol.com>
2025-09-30 12:46:38 +0530

    python package build command added

--- strands_agentcore/__init__.py
+++ strands_agentcore/__init__.py
@@ -1,6 +1,11 @@
-# strands_agentcore/__init__.py
 __version__ = "0.1.0"
 
-# re-export main small helpers for tests
-from .external_api import fetch_objects, create_agent_if_available  # noqa
-from .runtime_deployment import check_required_files, set_env_defaults  # noqa
+from .external_api import get_objects_api, create_agent, handler
+from .runtime_deployment import configure_and_launch
+
+__all__ = [
+    "get_objects_api",
+    "create_agent",
+    "handler",
+    "configure_and_launch",
+]
